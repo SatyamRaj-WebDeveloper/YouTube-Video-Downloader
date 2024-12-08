90bc84c030506e641d34bb14cc8dced1933f9ac8
SatyamRaj-wbedev <151543119+SatyamRaj-wbedev@users.noreply.github.com>
2024-12-08 12:03:51 +0530

    completed Project

--- Backend/ytapp/views.py
+++ Backend/ytapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,HttpResponse
 from rest_framework.decorators import api_view
 from django.http import JsonResponse,FileResponse
-from pytube import YouTube
+import yt_dlp
+import tempfile
 import os
 
 # Create your views here.
@@ -10,17 +11,27 @@ def donloadVideo(request):
     url = request.data.get('url')
     if url :
         try:
-            yt = YouTube(url)
-            print('Yt',yt)
-            stream = yt.streams.all()
-            if not stream:
-                print("No streams Available")
-            else:
-                print("Stream",stream)
-            file_path = stream.download()
-            print('file_path:',file_path)
-            return FileResponse(open(file_path, 'rb'), as_attachment=True, filename=stream.default_filename)
+            with tempfile.TemporaryDirectory() as temp_dir:
+                ydl_opts = {
+                'format': 'best',
+                'outtmpl': os.path.join(temp_dir, '%(title)s.%(ext)s'),
+            }
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(url, download=True)
+                file_path = ydl.prepare_filename(info)
+
+            # Open the file after the temp_dir block to ensure it's accessible
+            video_file = open(file_path, 'rb')
+            response = FileResponse(
+                video_file,
+                as_attachment=True,
+                filename=os.path.basename(file_path)
+            )
+            response['Content-Type'] = 'video/mp4'
+            response['Access-Control-Allow-Origin'] = '*'  # CORS
+            return response
         except Exception as e:
+            print(f"Exception{str(e)}")
             return JsonResponse({'message':f"Exception!!!{str(e)}"})
     elif url is None:
         return JsonResponse({"Message":"Did not recieve url in Backend"})
--- Backend/ytproject/settings.py
+++ Backend/ytproject/settings.py
@@ -54,9 +54,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_METHODS = [
     "GET",
